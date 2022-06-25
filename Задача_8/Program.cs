756a8642943f68c4245c7dc9a145b266cb1e6f08
ELENA <elenakand@icloud.com>
2022-06-25 23:44:09 +0300

    Нахождение ряда четных чисел

---
+++ Задача_8/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = int.Parse(numberStr);
+for (int count = 1; count <= number; count++)
+{
+    if (count%2 == 0)
+    {
+        Console.WriteLine(count);
+    }
+}
