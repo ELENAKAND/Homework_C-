b2ae9ba86153164685b643cb9ac4695db870c3c1
ELENA <elenakand@icloud.com>
2022-06-28 22:40:15 +0300

    Являеются ли два числа квадратом друг друга

--- Examples/Ex06/Program.cs
+++ Examples/Ex06/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// На вход 2 числа, является ли одно квадратом другого 
+Console.WriteLine("Введите первое число");
+string firstNumberStr = Console.ReadLine();
+int firstNumber = int.Parse(firstNumberStr); 
+Console.WriteLine("Введите второе число");
+string secondNumberStr = Console.ReadLine();
+int secondNumber = int.Parse(secondNumberStr);
+if (secondNumber * secondNumber == firstNumber)
+{
+    Console.WriteLine($"Первое число {firstNumber} является квадратом {secondNumber}");
+}
+if (firstNumber * firstNumber == secondNumber)
+{
+    Console.WriteLine($"Второе число {secondNumber} является квадратом {firstNumber}");
+}
+else
+{
+    Console.WriteLine("Числа не являются квадратом друг друга");
+}
+/* можно также записать
+if (first * first == second || secondNumber * second == first)
+|| значит "или"
+*/
