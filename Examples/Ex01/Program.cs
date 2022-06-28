8e39dc7a7f9b088238042cd5487ce52ea7bf0378
ELENA <elenakand@icloud.com>
2022-06-28 17:09:55 +0300

    Функция поиска max в массиве и вывод его индекса

--- Examples/Ex01/Program.cs
+++ Examples/Ex01/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿// Функция поиска max в массиве 
+int [] array = {12, 21, 33, 54, 53, 26, 97, 98};
+int n = array.Length;
+int find = 54;
+int index = 0;
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index); 
+    }
+    index++;
+   
+    
+}
