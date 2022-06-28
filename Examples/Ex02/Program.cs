34020fa2833c657e9fd3a2336af9891398a2caa7
ELENA <elenakand@icloud.com>
2022-06-28 18:36:17 +0300

    Еще раз выведение наибольшей цифры двузн числа

---
+++ Examples/Ex02/Program.cs
@@ -0,0 +1,14 @@
+﻿// На ввод случайное двузначное число - 
+// вывод наибольшей по значению цифры этого числа
+Random rnd = new Random(); //создали новый рандомный массив
+int number = rnd.Next(10, 100); //ввели переменную числа из массива
+Console.WriteLine(number);
+int firstNumber = number/10; //выделили первую цифру 2знач числа
+int secondNumber = number%10; //выделили вторую цифру 2знач числа
+if(firstNumber > secondNumber)
+    Console.WriteLine(firstNumber);
+if(secondNumber > firstNumber)
+    Console.WriteLine(secondNumber);
+if(firstNumber == secondNumber)
+    Console.WriteLine("Значения равны");
+    
