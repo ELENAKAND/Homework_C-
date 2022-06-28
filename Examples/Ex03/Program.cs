62c0dfb675610d0fc53feeadcb936e3169f54cf5
ELENA <elenakand@icloud.com>
2022-06-28 19:11:37 +0300

    Выведение первой и третьей цифры в 3знач числе

--- Examples/Ex03/Program.cs
+++ Examples/Ex03/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Random rnd = new Random();
+int number = rnd.Next(100, 1000);
+Console.WriteLine(number);
+int firstNumber = number /100; //выделение первой цифр
+int secondNumber = (number/10)%10; //выделение второй цифры
+int thirdNumber = number%10; //выделение третьей цифры
+Console.WriteLine("Полученное число {0}{1}", firstNumber, thirdNumber);
+
+
+
+
