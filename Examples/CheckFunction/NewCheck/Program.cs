8f148f606d8b26c84a972a41ccd0c346e1563a52
ELENA <elenakand@icloud.com>
2022-07-02 15:48:44 +0300

    Новая функция проверки

---
+++ Examples/CheckFunction/NewCheck/Program.cs
@@ -0,0 +1,22 @@
+﻿int GetFirstNumber(string message)
+{
+int result = 0;
+
+while(true)
+{
+Console.WriteLine(message);
+
+if(int.TryParse(Console.ReadLine(), out result))
+{
+break;
+}
+else
+{
+Console.Clear();
+Console.WriteLine("Вы ввели не число. Введите корректное число.");
+}
+}
+
+return result;
+}
+int number = GetFirstNumber("Введите число");
