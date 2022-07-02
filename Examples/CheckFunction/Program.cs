8281c7e751440983f58b6996a36689b44604ec42
ELENA <elenakand@icloud.com>
2022-07-02 13:51:10 +0300

    Функция проверки на ввод пользователем некорректной информации

---
+++ Examples/CheckFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿// Проверка на ввод пользователем неадекватной информации
+
+int GetNumber(string message);
+{
+
+    result = 0;
+    while (true)
+    {
+        Console.WriteLine("Введите число");
+        if (int.TryParse(Console.ReadLine(), out int result));
+        {
+            break;
+        }
+        else 
+        {
+            Console.Clear();
+            Console.WriteLine("Это не число. Введите число");
+        } 
+        
+    }   
+    return result;
+
+}
